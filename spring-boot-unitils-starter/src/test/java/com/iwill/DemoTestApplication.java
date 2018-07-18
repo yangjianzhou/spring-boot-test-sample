5bddf990a5cc89b31bbcd2d41cd57e6c4e66ca54
yangjianzhou <yangjianzhouctgu@126.com>
2018-07-18 17:38:16 +0800

    add code

--- spring-boot-unitils-starter/src/test/java/com/iwill/DemoTestApplication.java
+++ spring-boot-unitils-starter/src/test/java/com/iwill/DemoTestApplication.java
@@ -3,7 +3,6 @@ package com.iwill;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.test.context.ActiveProfiles;
 
 @ComponentScan({"com.iwill", "com.unitils.boot.autoconfigure"})
 @SpringBootApplication
